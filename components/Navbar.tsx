2f961cd875cc57645f05836e222fb3507dad56dd
Inioluwa Adeyinka <boluwatifeeri@gmail.com>
2026-04-21 09:15:38 +0100

    Update logo dimensions in Navbar component for improved responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar: React.FC = () => {
   return (
     <nav className="w-full px-4 md:px-12 py-6 md:py-12 flex justify-between items-center bg-white">
       <Link to="/" className="flex items-center">
-        <img src="/logo.png" alt="Plaited Logo" className="w-16 h-8 md:w-32 md:h-16 mr-2" />
+        <img src="/logo.png" alt="Plaited Logo" className="w-16 h-8 md:w-36 md:h-20 mr-2" />
       </Link>
       <div className="flex items-center gap-4 md:gap-8">
         <NavLink
